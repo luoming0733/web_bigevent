110f486ba056c40085f2c508bad639b867ff3c0b
lomi-luo <luoming0733@126.com>
2020-12-30 17:30:55 +0800

    feat:创建新文件

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,33 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
-})
+  options.url = "http://ajax.frontend.itheima.net" + options.url;
+
+  // 统一为有权限的接口，设置 headers 请求头
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+
+  // 统一为有权限的接口,设置 headers 请求头
+  options.headers = {
+    Authorization: localStorage.getItem("token") || "",
+  };
+
+  // 无论成功或者失败都会调用 complete 函数
+  options.complete = function (res) {
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      // 1. 强制清空 token
+      localStorage.removeItem("token");
+
+      // 2. 强制跳转到登录页面
+      location.href = "/login.html";
+    }
+  };
+});
