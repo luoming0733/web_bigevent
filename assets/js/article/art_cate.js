3780efcb87d626754015834c0360489ec2ce85fc
luoming0733 <luoming0733@126.com>
2021-03-06 10:38:55 +0800

    success

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,9 +1,10 @@
 $(function () {
   var layer = layui.layer;
+  var form = layui.form;
 
   initArtCateList();
 
-  //   获取文章分类列表
+  // 获取文章分类列表
   function initArtCateList() {
     $.ajax({
       method: "GET",
@@ -15,7 +16,7 @@ $(function () {
     });
   }
 
-  //为添加按钮绑定点击事件
+  // 为添加按钮绑定点击事件
   var indexAdd = null;
   $("#btnAddCate").on("click", function () {
     indexAdd = layer.open({
@@ -46,9 +47,9 @@ $(function () {
     });
   });
 
-  //通过代理的形式,为编辑按钮绑定事件
+  // 通过代理的形式，为 btn-edit 按钮绑定点击事件
   var indexEdit = null;
-  $("tbody").on("click", "#btnEditCate", function () {
+  $("tbody").on("click", ".btn-edit", function () {
     // 弹出一个修改文章的信息层
     indexEdit = layer.open({
       type: 1,
@@ -63,7 +64,47 @@ $(function () {
       url: "/my/article/cates/" + id,
       success: function (res) {
         form.val("form-edit", res.data);
+        console.log(res.data);
       },
+    });
+  });
+
+  // 通过代理的形式，为修改分类的表单绑定 submit 事件
+  $("body").on("submit", "#form-edit", function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/my/article/updatecate",
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("更新数据分类失败!");
+        }
+        layer.msg("更新数据分类成功!");
+        layer.close(indexEdit);
+        initArtCateList();
+      },
+    });
+  });
+
+  // 通过代理的形式，为删除按钮绑定 click 事件
+  $("tbody").on("click", ".btn-delete", function () {
+    var id = $(this).attr("data-id");
+    // 提示是否删除
+    layer.confirm("确认删除?", { icon: 3, title: "提示" }, function (index) {
+      $.ajax({
+        method: "GET",
+        url: "/my/article/deletecate/" + id,
+        success: function (res) {
+          console.log(res);
+          if (res.status !== 0) {
+            return layer.msg("删除分类失败！");
+          }
+          layer.msg("删除分类成功！");
+          layer.close(index);
+          initArtCateList();
+        },
+      });
     });
   });
 });
